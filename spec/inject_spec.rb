a86ba9cde99e72b70fdcf66a3d9095ed8ddbcd17
Hannah Knights <hannah.knights@hotmail.co.uk>
2013-12-06 14:29:16 +0000

    Finished the rewritign the inject method 'Injection'

--- spec/inject_spec.rb
+++ spec/inject_spec.rb
@@ -21,9 +21,25 @@ describe Array do
       expect(array.injection {|memo, obj| :block}).not_to raise_error
     end
 
-    
-
-
   end
-  
+
+  context "testing with examples" do
+
+    it "should be able to do addition" do 
+      array = [2,3,4]
+      expect(array.injection(1) {|sum, num| sum + num}).to eq(10)
+    end
+
+    it "should be able to do multiplication" do
+      array = [3,4,5]
+      expect(array.injection(2) {|product, num| product * num}).to eq(120)
+    end
+
+    it "should be able to take longer blocks" do
+      array = ["cat", "dog", "mouse", "flea"]
+      expect(array.inject {|animal, word| animal.length > word.length ? animal : word }).to eq("mouse")
+    end
+    
+  end
+ 
 end
