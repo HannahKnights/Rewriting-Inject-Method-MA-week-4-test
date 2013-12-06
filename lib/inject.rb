6861df1aa268185cf3b680870304e2a00085d30b
Hannah Knights <hannahknights@hotmail.co.uk>
2013-12-06 14:29:16 +0000

    Finished the rewritign the inject method 'Injection'

--- lib/inject.rb
+++ lib/inject.rb
@@ -1,7 +1,30 @@
  class Array
 
-  def injection(product = nil)
-    yield
+  def injection(product = 0)
+    value = product
+    self.each do |element|
+      yield(value, element)
+      value = yield(value, element)
+    end
+    value
   end
 
- end
+end
+
+
+
+
+
+
+
+
+
+# memo = self.first
+    
+#     self.each_with_index do |element, index|
+      
+#       yield(element)
+      
+#       return memo if index+1 == self.length
+      
+#       memo = yield memo, self[index+1]
